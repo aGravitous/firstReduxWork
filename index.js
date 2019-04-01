c36061602b9387cf6c04990b14996d22c431d256
meowcodes <meowcodes@gmail.com>
2019-04-01 11:26:15 -0700

    add event listeners

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 /** Initializing state and store */
 const INITIAL_STATE = {face: "♡´･ᴗ･`♡"};
 
-
-const FACES = {
-    happy: "♡´･ᴗ･`♡",
-    love: "♥(ˆ⌣ˆԅ)",
-    sad: "(╥_╥)",
-    sleepy: "꒰◍ᐡᐤᐡ◍꒱"
-}
-
-function moodReducer() {
+function moodReducer(state=INITIAL_STATE, action) {
     if (action.type === "HAPPY") {
         return { ...state, face: "♡´･ᴗ･`♡" };
     } else if (action.type === "LOVE") {
@@ -20,6 +12,36 @@ function moodReducer() {
         return { ...state, face: "꒰◍ᐡᐤᐡ◍꒱" };
     }
     return state;
-}
+};
 
 const store = Redux.createStore(moodReducer);
+
+window.onload = function() {
+    const faceElement = document.getElementById("faces")
+    const initialFace = store.getState().face;
+    faceElement.innerText = initialFace;
+
+    document.getElementById("happy").addEventListener("click", function() {
+        store.dispatch({type: "HAPPY"});
+        const newFace = store.getState().face;
+        faceElement.innerText = newFace;
+    });
+
+    document.getElementById("love").addEventListener("click", function() {
+        store.dispatch({type: "LOVE"});
+        const newFace = store.getState().face;
+        faceElement.innerText = newFace;
+    });
+
+    document.getElementById("sad").addEventListener("click", function() {
+        store.dispatch({type: "SAD"});
+        const newFace = store.getState().face;
+        faceElement.innerText = newFace;
+    });
+
+    document.getElementById("sleepy").addEventListener("click", function() {
+        store.dispatch({type: "SLEEPY"});
+        const newFace = store.getState().face;
+        faceElement.innerText = newFace;
+    });
+}
